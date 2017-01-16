31341b245be608c38ab4dfc5a4b1121fa3e6db78
peishenwu <peishen.wu@gmail.com>
2017-01-16 21:01:19 +0800

    rearrange

--- demo_plotTS.R
+++
@@ -1,5 +0,0 @@
-
-AirPassengers  %>%  as.data.frame %>%
-  mutate(row = sample(as.Date("2016-1-1")+1:300,144,replace = T))  %>%
-  group_by(row)  %>% summarize(count = sum(x),
-                               count2 = 2*mean(x))  %>% plot_timeseries(row,count,count2)
