54b5aa4f845fbf2c6bd517cc7e1fd993696d5066
peishenwu <peishen.wu@gmail.com>
2017-07-11 02:27:50 +0800

    tidyJunyi 1.2

--- install.R
+++ install.R
@@ -1,3 +1,8 @@
 ##installation code for tidyJunyi package
 library(devtools)
+
+##old version, which supports bigquery legacy SQL + dplyr 0.5 (not recommended)
 install_url("https://github.com/peishenwu/tidyJunyi/raw/master/tidyJunyi_0.76.tgz")
+
+##new version, which supports bigquery legacy SQL + dplyr 0.6 and above
+install_url("https://github.com/peishenwu/tidyJunyi/raw/master/tidyJunyi_1.2.tgz")
