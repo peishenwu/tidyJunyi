e7c4aaf4414ae82c7a5ec46c6039da46767ff08d
peishenwu <peishen.wu@gmail.com>
2017-07-11 02:31:56 +0800

    update install

--- install.R
+++ install.R
@@ -2,7 +2,7 @@
 library(devtools)
 
 ##old version, which supports bigquery legacy SQL + dplyr 0.5 (not recommended)
-install_url("https://github.com/peishenwu/tidyJunyi/raw/master/tidyJunyi_0.76.tgz")
+#install_url("https://github.com/peishenwu/tidyJunyi/raw/master/tidyJunyi_0.76.tgz")
 
-##new version, which supports bigquery legacy SQL + dplyr 0.6 and above
+##new version, which supports bigquery standard SQL + dplyr 0.6 and above
 install_url("https://github.com/peishenwu/tidyJunyi/raw/master/tidyJunyi_1.2.tgz")
